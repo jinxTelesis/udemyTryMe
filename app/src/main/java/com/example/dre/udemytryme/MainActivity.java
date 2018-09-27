cc61fb42bc2ac4783f4a8ea1d79a11aa56d58a47
jinxTelesis <Dremon7@hotmail.com>
2018-09-26 20:08:00 -0400

    initial commit

--- app/src/main/java/com/example/dre/udemytryme/MainActivity.java
+++ app/src/main/java/com/example/dre/udemytryme/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.dre.udemytryme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View windowView;
+    private Button tryMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
